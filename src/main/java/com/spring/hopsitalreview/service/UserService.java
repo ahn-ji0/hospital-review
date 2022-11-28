c23d669f3854e0b16156aaaf6348a3e7ed01478a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-28 16:24:21 +0900

    Update: user join request 처리하는 메소드 구체화

--- src/main/java/com/spring/hopsitalreview/service/UserService.java
+++ src/main/java/com/spring/hopsitalreview/service/UserService.java
@@ -1,12 +1,28 @@
 package com.spring.hopsitalreview.service;
 
+import com.spring.hopsitalreview.domain.User;
 import com.spring.hopsitalreview.domain.dto.UserDto;
 import com.spring.hopsitalreview.domain.dto.UserJoinRequest;
+import com.spring.hopsitalreview.domain.dto.UserJoinResponse;
+import com.spring.hopsitalreview.exception.ErrorCode;
+import com.spring.hopsitalreview.exception.HospitalReviewException;
+import com.spring.hopsitalreview.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
     public UserDto join(UserJoinRequest userJoinRequest){
-        return new UserDto("","","");
+        //회원 userName 중복 체크
+        //회원가입.save
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent(user -> {
+                    throw new HospitalReviewException(ErrorCode.DUPLICATE_USER_NAME,"유저 네임이 중복됩니다.");
+                });
+        User savedUser = userRepository.save(userJoinRequest.toEntity());
+        UserDto userDto = User.of(savedUser);
+        return userDto;
     }
 }
