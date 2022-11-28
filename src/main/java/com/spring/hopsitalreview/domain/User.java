38b6fb35d24dc254fa1c7c6065499e6bdc18e37c
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-28 16:23:32 +0900

    Add: entity에서 dto로 변환해주는 메소드 추가

--- src/main/java/com/spring/hopsitalreview/domain/User.java
+++ src/main/java/com/spring/hopsitalreview/domain/User.java
@@ -1,14 +1,21 @@
 package com.spring.hopsitalreview.domain;
 
+import com.spring.hopsitalreview.domain.dto.UserDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Entity
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +23,12 @@ public class User {
     private String userName;
     private String password;
     private String emailAddress;
+
+    public static UserDto of(User user){
+        return UserDto.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .email(user.getEmailAddress())
+                .build();
+    }
 }
