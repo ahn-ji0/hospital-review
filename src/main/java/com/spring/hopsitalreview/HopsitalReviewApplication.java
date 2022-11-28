8dfa64420cc53d1758068d323a3ff7d885f896fe
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-28 11:50:40 +0900

    init commit

---
+++ src/main/java/com/spring/hopsitalreview/HopsitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hopsitalreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopsitalReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HopsitalReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/hopsitalreview/HopsitalReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hopsitalreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopsitalReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
