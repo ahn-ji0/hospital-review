bfafe647cbc4add40a8adda9e9208149ec6e558a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-28 16:25:13 +0900

    Update: URL 경로 수정

--- src/main/java/com/spring/hopsitalreview/controller/UserController.java
+++ src/main/java/com/spring/hopsitalreview/controller/UserController.java
@@ -6,10 +6,7 @@ import com.spring.hopsitalreview.domain.dto.UserJoinResponse;
 import com.spring.hopsitalreview.response.Response;
 import com.spring.hopsitalreview.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -17,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest){
         UserDto userDto = userService.join(userJoinRequest);
-        return Response.success(new UserJoinResponse());
+        return Response.success(new UserJoinResponse(userDto.getUserName(),userDto.getEmail()));
     }
 }
