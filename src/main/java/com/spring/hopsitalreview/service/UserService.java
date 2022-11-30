4571381259c28184ca45e33ef9bbca792c305dda
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-30 13:22:14 +0900

    Update: 인코딩된 password로 저장하도록 수정

--- src/main/java/com/spring/hopsitalreview/service/UserService.java
+++ src/main/java/com/spring/hopsitalreview/service/UserService.java
@@ -8,20 +8,21 @@ import com.spring.hopsitalreview.exception.ErrorCode;
 import com.spring.hopsitalreview.exception.HospitalReviewException;
 import com.spring.hopsitalreview.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
     public UserDto join(UserJoinRequest userJoinRequest){
         //회원 userName 중복 체크
-        //회원가입.save
         userRepository.findByUserName(userJoinRequest.getUserName())
                 .ifPresent(user -> {
                     throw new HospitalReviewException(ErrorCode.DUPLICATE_USER_NAME,"유저 네임이 중복됩니다.");
                 });
-        User savedUser = userRepository.save(userJoinRequest.toEntity());
+        User savedUser = userRepository.save(userJoinRequest.toEntity(encoder.encode(userJoinRequest.getPassword())));
         UserDto userDto = User.of(savedUser);
         return userDto;
     }
