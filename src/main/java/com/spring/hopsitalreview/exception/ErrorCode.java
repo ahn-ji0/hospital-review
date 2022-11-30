9d38fc18ef280c79f44196f33bf714756db224e1
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-30 15:29:07 +0900

    Add: INVALID_PASSWORD 추가

--- src/main/java/com/spring/hopsitalreview/exception/ErrorCode.java
+++ src/main/java/com/spring/hopsitalreview/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     DUPLICATE_USER_NAME(HttpStatus.CONFLICT, "USER NAME CONFLICT"),
-    NOT_FOUND(HttpStatus.NOT_FOUND,"USER NOT FOUND");
+    NOT_FOUND(HttpStatus.NOT_FOUND,"USER NOT FOUND"),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST,"PASSWORD INVALID");
 
     private HttpStatus httpStatus;
     private String message;
