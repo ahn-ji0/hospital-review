d4d3c61b1ba6bab6c332d8e44f8dadfc81cc1f13
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-30 14:46:52 +0900

    Add: NOT_FOUND 추가

--- src/main/java/com/spring/hopsitalreview/exception/ErrorCode.java
+++ src/main/java/com/spring/hopsitalreview/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    DUPLICATE_USER_NAME(HttpStatus.CONFLICT, "USER NAME CONFLICT");
+    DUPLICATE_USER_NAME(HttpStatus.CONFLICT, "USER NAME CONFLICT"),
+    NOT_FOUND(HttpStatus.NOT_FOUND,"USER NOT FOUND");
 
     private HttpStatus httpStatus;
     private String message;
