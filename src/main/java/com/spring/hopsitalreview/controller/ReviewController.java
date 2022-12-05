1b146ed30c320841b2e527abe22395e77d21024f
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-05 16:08:06 +0900

    Update: authentication 파라미터 추가

--- src/main/java/com/spring/hopsitalreview/controller/ReviewController.java
+++ src/main/java/com/spring/hopsitalreview/controller/ReviewController.java
@@ -1,14 +1,18 @@
 package com.spring.hopsitalreview.controller;
 
 import com.spring.hopsitalreview.domain.dto.ReviewCreateRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@Slf4j
 public class ReviewController {
 
     @PostMapping
-    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest){
+    public String write(@RequestBody ReviewCreateRequest reviewCreateRequest, Authentication authentication){
+        log.info("Controller user:{}", authentication.getName());
         return "리뷰 등록에 성공";
     }
 }
