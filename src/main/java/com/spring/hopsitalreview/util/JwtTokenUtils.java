31d402fda45c74268675a91af8056e4efddb9d0a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-05 14:16:16 +0900

    Add: Expired 되었는지 확인하는 메소드 추가

--- src/main/java/com/spring/hopsitalreview/util/JwtTokenUtils.java
+++ src/main/java/com/spring/hopsitalreview/util/JwtTokenUtils.java
@@ -7,6 +7,13 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 
 public class JwtTokenUtils {
+    private static Claims extractClaims(String token, String key){
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
+    public static boolean isExpired(String token, String secretKey){
+        Date expiredDate = extractClaims(token,secretKey).getExpiration();
+        return expiredDate.before(new Date());
+    }
     public static String createToken(String userName, String key, long expireTimeMs){
         Claims claims = Jwts.claims();
         claims.put("userName",userName);
