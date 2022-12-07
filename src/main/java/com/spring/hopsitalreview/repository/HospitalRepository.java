3e76551a89e1d02cb45f0ef6ae83411bfbf29c4a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-07 14:22:45 +0900

    Add: findByHospitalName

--- src/main/java/com/spring/hopsitalreview/repository/HospitalRepository.java
+++ src/main/java/com/spring/hopsitalreview/repository/HospitalRepository.java
@@ -3,5 +3,8 @@ package com.spring.hopsitalreview.repository;
 import com.spring.hopsitalreview.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HospitalRepository extends JpaRepository<Hospital,Long> {
+    public Optional<Hospital> findByHospitalName(String hospitalName);
 }
