835c2042a924b969c1a319d9fd7a9558be36965a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-07 13:41:58 +0900

    Add: entity를 dto로 변환하는 메소드 구현

--- src/main/java/com/spring/hopsitalreview/domain/entity/Hospital.java
+++ src/main/java/com/spring/hopsitalreview/domain/entity/Hospital.java
@@ -1,5 +1,6 @@
 package com.spring.hopsitalreview.domain.entity;
 
+import com.spring.hopsitalreview.domain.dto.HospitalResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,4 +24,12 @@ public class Hospital {
 
     @Column(name = "road_name_address")
     private String roadNameAddress;
+
+    public static HospitalResponse of(Hospital hospital) {
+        return HospitalResponse.builder()
+                .id(hospital.getId())
+                .hospitalName(hospital.getHospitalName())
+                .roadNameAddress(hospital.getRoadNameAddress())
+                .build();
+    }
 }
