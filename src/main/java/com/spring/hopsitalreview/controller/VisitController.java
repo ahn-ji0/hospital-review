2f400afcdcb70e93bc1f1df4b4f0844295090aa1
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-07 15:29:03 +0900

    Add: 전체 visit 조회, user id로 조회, hospital id로 조회하는 GET 메소드 추가

--- src/main/java/com/spring/hopsitalreview/controller/VisitController.java
+++ src/main/java/com/spring/hopsitalreview/controller/VisitController.java
@@ -3,16 +3,17 @@ package com.spring.hopsitalreview.controller;
 import com.spring.hopsitalreview.domain.dto.ReviewCreateRequest;
 import com.spring.hopsitalreview.domain.dto.VisitCreateRequest;
 import com.spring.hopsitalreview.domain.dto.VisitCreateResponse;
+import com.spring.hopsitalreview.domain.dto.VisitResponse;
 import com.spring.hopsitalreview.domain.entity.Visits;
 import com.spring.hopsitalreview.response.Response;
 import com.spring.hopsitalreview.service.VisitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/visit")
@@ -22,7 +23,23 @@ public class VisitController {
     private final VisitService visitService;
     @PostMapping
     public Response<VisitCreateResponse> write(@RequestBody VisitCreateRequest visitCreateRequest, Authentication authentication){
-        Visits visits = visitService.create(visitCreateRequest, authentication.getName());
-        return Response.success(Visits.of(visits));
+        VisitCreateResponse visits = visitService.create(visitCreateRequest, authentication.getName());
+        return Response.success(visits);
+    }
+    @GetMapping
+    public Response<List<VisitResponse>> list(){
+        List<VisitResponse> visitResponseList = visitService.list();
+        return Response.success(visitResponseList);
+    }
+    @GetMapping("/users/{id}")
+    public Response<List<VisitResponse>> listByUser(@PathVariable Long id){
+        List<VisitResponse> visitResponseList = visitService.listByUser(id);
+        return Response.success(visitResponseList);
+    }
+
+    @GetMapping("/hospitals/{id}")
+    public Response<List<VisitResponse>> listByHospital(@PathVariable Long id){
+        List<VisitResponse> visitResponseList = visitService.listByHospital(id);
+        return Response.success(visitResponseList);
     }
 }
