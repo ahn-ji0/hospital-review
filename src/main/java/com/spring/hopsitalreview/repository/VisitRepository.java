fd9ec50261e64431992940d0b29a27354c894460
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-07 15:28:21 +0900

    Add: findByUser, findByHospital 추가

--- src/main/java/com/spring/hopsitalreview/repository/VisitRepository.java
+++ src/main/java/com/spring/hopsitalreview/repository/VisitRepository.java
@@ -1,7 +1,13 @@
 package com.spring.hopsitalreview.repository;
 
+import com.spring.hopsitalreview.domain.entity.Hospital;
+import com.spring.hopsitalreview.domain.entity.User;
 import com.spring.hopsitalreview.domain.entity.Visits;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VisitRepository extends JpaRepository<Visits,Long> {
+    List<Visits> findByUser(User user);
+    List<Visits> findByHospital(Hospital hospital);
 }
