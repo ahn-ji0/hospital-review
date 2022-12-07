16d8af08247aa076a995c63a956291f316442713
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-07 13:28:13 +0900

    Visit Entity

---
+++ src/main/java/com/spring/hopsitalreview/domain/entity/Visits.java
@@ -0,0 +1,33 @@
+package com.spring.hopsitalreview.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Builder
+public class Visits {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Hospital hospital;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Disease disease;
+
+
+}
